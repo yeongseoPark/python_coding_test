bae136faefe22d5b56a543a324779acec42b1d07
Park YengSeo <1park4170@gmail.com>
2022-07-05 20:26:48 +0900

    7/5

---
+++ implement/LongestPrefix.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        
+        if len(strs) == 1:
+            return strs[0]
+
+        strs.sort(key=len)
+
+        print(strs)
+        minlen = len(strs[0])
+
+        if minlen == 0:
+            return ""
+        
+        newstrs = [i[:minlen] for i in strs]
+        
+        pre_len = []
+        for i in newstrs:
+            tmp = newstrs[:]
+            tmp.remove(i)
+            for k in tmp:
+                comm = 0
+                for j in range(minlen):
+                    if i[j] == k[j]:
+                        comm += 1
+                    elif i[j] != k[j]:
+                        pre_len.append(comm)
+                        break
+
+                    if j == minlen-1:
+                        pre_len.append(comm)
+        
+        print(pre_len)
+        if min(pre_len) == 0:
+            return ""
+        else:
+            return newstrs[0][:min(pre_len)]
+
+        
+print(Solution().longestCommonPrefix(["a","ab","abac","adfsdfsd"]))
