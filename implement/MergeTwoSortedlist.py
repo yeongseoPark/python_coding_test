6fb43e1813db7f63bbd60abdb4bb3084cfb6a344
Park YengSeo <1park4170@gmail.com>
2022-07-08 18:29:11 +0900

    7/8

---
+++ implement/MergeTwoSortedlist.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        cur = dummy= ListNode() # dummy : 새로운 리스트의 head저장, cur : while문 안에서 현재의 마지막 요소를 저장하는데 사용
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next   = list1  # 포인터 옮겨주고
+                cur        = cur.next # cur도 바꿔준다 -> cur을 바꿔주지 않으면 linkedList가 앞으로 나아가지 않음
+                list1      = list1.next # 연결리스트는 앞에서 한칸 당겨줌
+            else:
+                cur.next   = list2 
+                cur        = cur.next
+                list2      = list2.next
+        
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            cur      = cur.next
+        
+        
+        return dummy.next # dummy는 while문을 돌며 cur에 추가되는 first node를 가리키고 있다
