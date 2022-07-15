1e74c878eb716dd439ba140a68a90b856d2e6dbd
YSEO <1park4170@gmail.com>
2022-07-15 14:51:51 +0900

    Update MergeTwoSortedlist.py

--- implement/MergeTwoSortedlist.py
+++ implement/MergeTwoSortedlist.py
@@ -6,12 +6,14 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        cur = dummy= ListNode() # dummy : 새로운 리스트의 head저장, cur : while문 안에서 현재의 마지막 요소를 저장하는데 사용
-        while list1 and list2:
-            if list1.val < list2.val:
-                cur.next   = list1  # 포인터 옮겨주고
+        cur = dummy= ListNode() # dummy로 merge된 연결리스트의 처음을 가리킨다
+        
+        while list1 and list2: # list1과 list2는 연결리스트를 가리키는 "포인터"
+            
+            if list1.val < list2.val: # 오름차순이니까 더 작은 것 선택
+                cur.next   = list1  
                 cur        = cur.next # cur도 바꿔준다 -> cur을 바꿔주지 않으면 linkedList가 앞으로 나아가지 않음
-                list1      = list1.next # 연결리스트는 앞에서 한칸 당겨줌
+                list1      = list1.next # list1의 맨 앞 요소를 선택했으니, list1을 가리키는 포인터도 한칸 나아간다
             else:
                 cur.next   = list2 
                 cur        = cur.next
